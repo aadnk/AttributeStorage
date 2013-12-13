b5c8b0eba57e508a018a483097e41d5c91512c26
Kristian S. Stangeland <kr_stang@hotmail.com>
2013-12-13 05:50:48 +0100

    Initial commit.

---
+++ AttributeStorage/src/main/java/com/comphenix/attribute/App.java
@@ -0,0 +1,11 @@
+package com.comphenix.attribute;
+
+/**
+ * Hello world!
+ * 
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ AttributeStorage/src/test/java/com/comphenix/attribute/AppTest.java
@@ -0,0 +1,15 @@
+package com.comphenix.attribute;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	@Test
+	public void testApp() {
+		assertTrue(true);
+	}
+}
